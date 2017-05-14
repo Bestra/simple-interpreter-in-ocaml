993036a83a2ca5daaf5a5afd047f1fad1ca3259c
Chris Westra <cwestra@gmail.com>
2017-05-13 20:48:39 -0400

    Add basic error messaging

--- interpreter.ml
+++ interpreter.ml
@@ -9,19 +9,19 @@ let perform_op a op b =
                 No op, next token is Operator *)
 let rec eval_expr accum op tokens =
   match tokens with
-  | [] -> accum
+  | [] -> Ok accum
   | hd :: tl ->
     match (op, hd) with
     | (Some o, Token.Integer i) ->
       eval_expr (perform_op accum o i) None tl
     | (None, Token.Operator o) ->
       eval_expr accum (Some o) tl
-    | _ -> eval_expr accum op tl
+    | _ -> Error "syntax error in evaluation. try again"
 
 let  evaluate_expr tokens =
   match Token.remove_whitespace(tokens) with
   | Token.Integer i :: Token.Operator o :: tl -> eval_expr i (Some o) tl
-  | _ -> 0
+  | _ -> Error "syntax error in tokens. try again"
 
 let rec build_expr tokens chars =
   match chars with
@@ -31,9 +31,13 @@ let rec build_expr tokens chars =
     build_expr (List.append tokens [new_token]) remaining_chars
 
 let expr text =
-  let result = evaluate_expr (build_expr [] text) in
-  printf "%i" result;
-  print_newline ()
+  match evaluate_expr (build_expr [] text) with
+  | Ok result ->
+    printf "%i" result;
+    print_newline ()
+  | Error e ->
+    printf "%s" e;
+    print_newline ()
 
 
 let interpret text =
