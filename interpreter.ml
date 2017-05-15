a7965d87abc7e9c53c4f9cb153d1beca3b97c4e8
Chris Westra <cwestra@gmail.com>
2017-05-15 13:46:22 -0400

    Rework interpreter

--- interpreter.ml
+++ interpreter.ml
@@ -7,43 +7,59 @@ let perform_op a op b =
 
 (* valid cases: Some op, next token is integer
                 No op, next token is Operator *)
-let rec eval_expr accum op tokens =
-  match tokens with
-  | [] -> Ok accum
-  | hd :: tl ->
-    match (op, hd) with
-    | (Some o, Token.Integer i) ->
-      eval_expr (perform_op accum o i) None tl
-    | (None, Token.Operator o) ->
-      eval_expr accum (Some o) tl
-    | _ -> Error "syntax error in evaluation. try again"
 
-let  evaluate_expr tokens =
-  match Token.remove_whitespace(tokens) with
-  | Token.Integer i :: Token.Operator o :: tl -> eval_expr i (Some o) tl
-  | _ -> Error "syntax error in tokens. try again"
+let factor tokens =
+  match tokens with
+  | Token.Integer i :: tl -> Ok (i, tl)
+  | hd :: _ -> Error (Printf.sprintf "next token %s is not an integer" (Token.to_string hd))
+  | [] -> Error "no remaining tokens to factor"
+
+let term tokens =
+  let rec term' acc t =
+    match t with
+    | Token.Operator o :: tl ->
+      (match factor tl with
+       | Ok (next_factor_result, ts) ->
+         term' (perform_op acc o next_factor_result) ts
+       | Error _ as e -> e
+      )
+    | _ -> Ok (acc, t)
+  in
+  match factor tokens with
+  | Ok (i, t) -> term' i t
+  | Error _ as e -> e
 
 let expr tokens =
-  match evaluate_expr tokens with
-  | Ok result ->
-    printf "%i" result;
-    print_newline ()
-  | Error e ->
-    printf "%s" e;
-    print_newline ()
+  let rec expr' acc t =
+    match t with
+    | Token.Operator o :: tl ->
+      (match term tl with
+       | Ok (next_term_result, ts) ->
+         expr' (perform_op acc o next_term_result) ts
+       | Error _ as e -> e
+      )
+    | _ -> Ok acc
+  in
+  match term tokens with
+  | Ok (result, remaining_tokens) -> expr' result remaining_tokens
+  | Error _ as e -> e
 
 let interpret text =
   printf "you input %s" text;
   print_newline ();
-  expr (Token.remove_whitespace (Lexer.tokenize text))
+  let tokens = (Token.remove_whitespace (Lexer.tokenize text)) in
+  expr tokens
 
 let rec read_and_interpret () =
   let line = In_channel.input_line In_channel.stdin in
   match line with
-  | None -> printf "done"
-  | Some x -> interpret x; read_and_interpret ()
+  | None -> print_endline "done"
+  | Some x ->
+    (match interpret x with
+     | Ok i -> printf "The result is %i" i; print_newline();
+     | Error s -> print_endline s);
+    read_and_interpret ()
 
 let () =
-  printf "Calculator:";
-  print_newline ();
+  print_endline "Calculator:";
   read_and_interpret ()
